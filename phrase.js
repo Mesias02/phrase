b12c1c33d3499e1a2d090ded881cbdc47d6f7874
Mesias02 <146044728+Mesias02@users.noreply.github.com>
2024-01-18 03:03:01 -0500

    Delete phrase.js

--- phrase.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-let phrase = 'La programación web es fundamental para el progreso';
-let wordsArray = phrase.split(' ');
-let numberOfWords = wordsArray.length;
-
-alert(`La frase tiene ${numberOfWords} palabras.`);
-
