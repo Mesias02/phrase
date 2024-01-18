4bfd4413a76da17ba4b26f026ed3eccdf2695e67
Mesias02 <146044728+Mesias02@users.noreply.github.com>
2024-01-18 02:57:53 -0500

    Add files via upload

---
+++ phrase/phrase.js
@@ -0,0 +1,8 @@
+'use strict'
+
+let phrase = 'La programación web es fundamental para el progreso';
+let wordsArray = phrase.split(' ');
+let numberOfWords = wordsArray.length;
+
+alert(`La frase tiene ${numberOfWords} palabras.`);
+
